db3e6a020f8a8e582f0d81cb099e102cc673d378
dreamdhub48 <118086609+dreamdhub48@users.noreply.github.com>
2023-02-20 22:30:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [users, setUsers] = useState([]);
+
+  const loadUser = async () => {
+    const response = await fetch("https://reqres.in/api/users?page=1");
+    const jsonResponse = await response.json();
+    setUsers(jsonResponse.data);
+  };
+  return (
+    <div>
+      <div className="header">
+        <h1 className="head-title">TASK 2</h1>
+        <button className="head-btn" onClick={loadUser}>
+          Get User
+        </button>
+      </div>
+      <div className="users-space">
+        {users.map(({ id, email, first_name, last_name, avatar }) => (
+          <div className="card" style={{ width: "12rem" }}>
+            <div className="main">
+              <img className="card-img-top" src={avatar} alt="Card image cap" />
+              <div className="card-body">
+                <h5 className="card-id" key={id}>
+                  ID: {id}
+                </h5>
+                <h5
+                  className="card-title"
+                  key={id}
+                >{`${first_name} ${last_name}`}</h5>
+                <p className="card-text" key={id}>
+                  {email}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
